4c59367f5f300eaf75bc1aea8c966af802f92ae4
taralalukasz <taralalukasz123@gmail.com>
2023-12-18 15:51:41 +0100

    Chapter 3.6 extracted httprouter error handling

--- cmd/api/router.go
+++ cmd/api/router.go
@@ -8,6 +8,9 @@ import (
 
 func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
+	//httprouter lets us configure default handlers in case of common issues like server error or not found page.
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
     router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
     router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
     router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
