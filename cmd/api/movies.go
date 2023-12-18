b9549c449e9c9b391c05189f7d11400876734103
taralalukasz <taralalukasz123@gmail.com>
2023-12-18 16:54:47 +0100

    Chapter 4.1 added Decode/Unmarshall logic to create movie handler

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"time"
 
@@ -9,7 +11,55 @@ import (
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create new movie")
+	// fields must be exported (i.e. start with Capital Letters)
+
+	var input struct {
+		Title   string   `json:"title"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+		Year    int32    `json:"year"`
+	}
+
+	// &input doesn't have to be initialized - it's already initialized above with empty values
+	// decoder check struct tag names above. If they don't match - it tries to match key names case-insensitive
+	// json values, which can't be mapped, will be silently ignored 
+	// important - it MUST BE non-nil pointer
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	fmt.Fprintf(w, "%+v\n", input)
+}
+
+//this is exactly the same as json.Decode(). But more  verbose and less efficient 80% more memory 
+// thus we are going to use createMovieHandler method later
+func (app *application) createMovieHandlerWithUnmarshall(w http.ResponseWriter, r *http.Request) {
+	// fields must be exported (i.e. start with Capital Letters)
+
+	var input struct {
+		Title   string   `json:"title"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+		Year    int32    `json:"year"`
+	}
+
+	body, err:= io.ReadAll(r.Body)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	//same here - target must be a struct pointer. What actually makes sense, as this method doesn't return anything  
+	err = json.Unmarshal(body, &input)
+	if err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+
+
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
